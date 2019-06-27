1f28df9bce4a304ce1ae1133c8ed3a950488b261
Nelson E. Aleluya Quispe <aleluyaerwin@gmail.com>
2019-06-27 17:52:52 -0400

    jueves 27 de junio de 2019

---
+++ resources/views/productos/delete.blade.php
@@ -0,0 +1,11 @@
+@extends('../layouts.plantilla')
+
+@section("cabecera")
+
+
+@section("contenido")
+
+<form method="post">
+    <input type="text" name="nombre_articulo">
+    <input type="submit" name="enviar" value="enviar">
+</form>
